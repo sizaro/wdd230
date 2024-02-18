ebf2e12b51071bb6b47196ba57c92ba2d1dbab97
sizaro <spartansiza@gmail.com>
2024-02-17 22:01:07 -0800

    done

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -2,3 +2,29 @@ let lastUpdated = document.getElementById("lastUpdated");
 lastUpdated.innerHTML = `Last Modified ${(document.lastModified)}`;
 let currentYear = document.getElementById("currentYear");
 currentYear.textContent = "2024";
+
+let tempInput = document.querySelector("#tempValue");
+let speedInput = document.querySelector("#speedValue");
+let windchill = document.querySelector("#windchill");
+
+function calculateWindChill(temp, speed) {
+    if (temp <= 50 && speed > 3.0) {
+        let windchillResults = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+        return windchillResults.toFixed(2);
+    } else {
+        return "N/A";
+    }
+}
+
+function updateWindChill() {
+    let tempValue = parseFloat(tempInput.value);
+    let speedValue = parseFloat(speedInput.value);
+
+    windchill.textContent = calculateWindChill(tempValue, speedValue);
+}
+
+tempInput.addEventListener('input', updateWindChill);
+speedInput.addEventListener('input', updateWindChill);
+
+updateWindChill();
+
