1bcdbe931dffaa3cb86ff2c4728890683266e502
sizaro <spartansiza@gmail.com>
2024-02-24 18:49:55 -0800

    theme

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -22,16 +22,41 @@ humburgerIcon.addEventListener("click", () => {
 })
 
 const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
+const body = document.querySelector("#body");
+const header = document.querySelector(".header");
+const footer = document.querySelector("#footer");
+const nav = document.querySelector(".nav");
+const cad = document.querySelector(".cad");
+const cad1 = document.querySelector(".cad1");
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("🕶️")) {
-        main.style.background = "#000";
-        main.style.color = "#fff";
+        body.style.background = "#000";
+        body.style.color = "#fff";
+        header.style.background = "#000";
+        header.style.color = "#fff";
+        footer.style.background = "#000";
+        footer.style.color = "#fff";
+        nav.style.background = "#000";
+        nav.style.color = "#fff";
+        cad.style.background = "#000";
+        cad.style.color = "fff";
+        cad1.style.background = "#000";
+        cad1.style.color = "#fff";
         modeButton.textContent = "🔆";
     } else {
-        main.style.background = "#eee";
-        main.style.color = "#000";
+        body.style.background = "white";
+        body.style.color = "black";
+        header.style.background = "blue";
+        header.style.color = "#white";
+        footer.style.background = "blue";
+        footer.style.color = "white";
+        nav.style.background = "#eee";
+        nav.style.color = "#000";
+        cad.style.background = "blue";
+        cad.style.color = "white";
+        cad1.style.background = "blue";
+        cad1.style.color = "white";
         modeButton.textContent = "🕶️";
     }
 });
@@ -46,7 +71,7 @@ numberOfVisits++;
 localStorage.setItem("visits-num", numberOfVisits);
 
 
-if (numberOfVisits !== 0) {
+if (numberOfVisits !== 1) {
     paraDisplay.textContent = `Page visits: ${numberOfVisits}`;
 }
 else {
