aa8c5d940ebb107c26266d97ab26f3fc3ca92905
sizaro <spartansiza@gmail.com>
2024-03-06 03:56:14 -0800

    di.........

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,1 +1,12 @@
 console.log("script loaded");
+
+const url = ""
+let card = document.querySelector("#cards");
+let section = document.createElement("section");
+let heading = document.createElement("h1");
+let image = document.createElement("img");
+let paragraph1 = document.createElement("p");
+let paragraph2 = document.createElement("p");
+let paragraph3 = document.createElement("p");
+let paragraph4 = document.createElement("p");
+
