2b40941451d2599818ae7a6b49a73d7b01b14c5f
sizaro <spartansiza@gmail.com>
2024-03-16 10:33:50 -0700

    now..

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,1 +1,77 @@
 document.getElementById('formDate').value = Date.now();
+
+let lastUpdated = document.getElementById("lastUpdated");
+lastUpdated.innerHTML = `Last Modified ${(document.lastModified)}`;
+let currentYear = document.getElementById("currentYear");
+currentYear.textContent = "2024";
+
+document.addEventListener("DOMContentLoaded", function () {
+    let humburgerNav = document.querySelector("nav");
+    humburgerNav.classList.add("humburger")
+    let humburgerButton = document.querySelector("#humburger");
+    if (humburgerNav.classList.contains("humburger")) {
+        humburgerButton.textContent = "☰";
+    }
+    if (humburgerNav.classList.contains("show")) {
+        humburgerButton.textContent = "×";
+    }
+    humburgerButton.addEventListener("click", () => {
+        if (humburgerNav.classList.contains("humburger")) {
+            humburgerNav.classList.remove("humburger");
+            humburgerNav.classList.add("show");
+            humburgerButton.textContent = "×";
+        }
+
+        else {
+            humburgerNav.classList.add("humburger");
+            humburgerNav.classList.remove("show");
+            humburgerButton.textContent = "☰";
+        }
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    let darkMode = document.querySelector("body");
+    let darkModeButton = document.querySelector("#mode");
+
+    if (darkModeButton) {
+        darkModeButton.textContent = "🌞";
+    }
+
+    if (darkMode.classList.contains("dark-mode")) {
+        darkModeButton.textContent = "🌙";
+    }
+
+    darkModeButton.addEventListener("click", () => {
+        darkMode.classList.toggle("dark-mode");
+
+        if (darkMode.classList.contains("dark-mode")) {
+            document.querySelector("#mode").style.backgroundColor = "white"
+            document.querySelector(".joinButton").style.backgroundColor = "black";
+            document.querySelectorAll("section").forEach(section => {
+                section.style.backgroundColor = "black";
+                section.style.color = "white";
+            }
+            )
+            darkModeButton.textContent = "🌙";
+            darkModeButton.style.backgroundColor = "black";
+        }
+        else {
+            darkModeButton.style.setProperty('--before-content-mode', '');
+            document.querySelector(".joinButton").style.backgroundColor = "#3f84c0";
+            document.querySelectorAll("section").forEach(section => {
+                section.style.backgroundColor = "#3f84c0";
+                section.style.color = "white";
+            }
+            )
+            darkModeButton.textContent = "🌞";
+            darkModeButton.style.backgroundColor = "white";
+
+        }
+
+    })
+
+})
+
+
+
--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,26 +1,3 @@
-
-console.log("Script loaded!");
-let visitDisplay = document.querySelector(".pageVisits");
-
-let lastVisit = localStorage.getItem("lastVisit");
-
-let currentDate = new Date();
-
-let timeDifference = currentDate.getTime() - lastVisit;
-
-let daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-
-localStorage.setItem("lastVisit", currentDate.getTime());
-
-if (lastVisit === 0) {
-    visitDisplay.textContent = "Welcome! Let us know if you have any questions.";
-    document.querySelector(".pageVisit").style
-} else if (daysDifference === 0) {
-    visitDisplay.textContent = "Back so soon! Awesome!";
-} else {
-    visitDisplay.textContent = `You last visited ${daysDifference} ${daysDifference === 1 ? 'day' : 'days'} ago.`;
-};
-
 let lastUpdated = document.getElementById("lastUpdated");
 lastUpdated.innerHTML = `Last Modified ${(document.lastModified)}`;
 let currentYear = document.getElementById("currentYear");
@@ -95,19 +72,4 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 
 
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    let datefun = () => {
-        console.log("Script is running!");
-        let lastMod = document.getElementById("lastModified");
-        lastMod.innerHTML = `Last Modification: ${(document.lastModified)}`;
-
-        let currentYear = document.getElementById("currentYear");
-        currentYear.textContent = "2024";
-    };
-
-    datefun();
-});
 
