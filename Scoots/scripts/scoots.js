520cb49c4c8ec0b7f72ab4e4ff981c36cf90487b
sizaro <spartansiza@gmail.com>
2024-04-10 11:00:23 -0700

    final

--- Scoots/scripts/scoots.js
+++ Scoots/scripts/scoots.js
@@ -142,32 +142,156 @@ document.addEventListener("DOMContentLoaded", function () {
 
     })
 
-})/*
+})
+
+
 const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=0.4475271339059875&lon=33.20316576377895&appid=1b38869f1c347522d45b8c33cd852ef5";
-let weatherForecast = document.querySelector(".weatherForecast");
-let tempNow = document.querySelector("#currentTempValue");
-let weatherNowDesc = document.querySelector("#weatherDescription");
-let imageicon = document.createElement("img");
-weatherForecast.appendChild(imageicon);
+let tempNow = document.querySelector(".current-temperature");
+let humidNow = document.querySelector(".current-humidity");
+let weatherNowDesc = document.querySelector(".description");
 
 async function apiweatherFetch(url) {
     let response = await fetch(url);
     if (response.ok) {
         let data = await response.json();
         displayweatherNow(data);
+        maxTemp(data);
     }
 }
 
 
+
+function maxTemp(data) {
+    let weatherBanner = document.querySelector("#weatherInformation");
+    let bodyItem = document.querySelector("body")
+    let weatherdata = data.main.temp_max;
+    let weatherPara = document.createElement("p");
+    let weatherButton = document.createElement("button");
+    weatherPara.textContent = weatherdata;
+    weatherButton.textContent = `X`;
+    weatherPara.classList.add("weather-para");
+    weatherButton.classList.add("weather-button");
+    weatherBanner.appendChild(weatherPara);
+    weatherBanner.appendChild(weatherButton);
+    weatherBanner.style.display = "flex";
+    weatherBanner.style.justifyContent = "center";
+    weatherBanner.style.alignItems = "center";
+    weatherBanner.style.color = "black";
+
+    weatherButton.addEventListener("click", () => {
+        weatherBanner.style.display = "none";
+    })
+}
+
+
+/*
+function maxTemp(data) {
+    addEventListener("DOMContentLoaded", () => {
+        let weatherBanner = document.querySelector("#weatherInformation");
+        let weatherdata = data.main.temp_max;
+        let weatherPara = document.createElement("p");
+        let weatherButton = document.createElement("button");
+        weatherPara.textContent = weatherdata;
+        weatherButton.textContent = `X`;
+        weatherPara.classList.add("weather-para");
+        weatherButton.classList.add("weather-button");
+        weatherBanner.appendChild(weatherPara);
+        weatherBanner.appendChild(weatherButton);
+        weatherBanner.style.display = "flex";
+        weatherBanner.style.color = "black";
+
+        weatherButton.addEventListener("click", () => {
+            weatherBanner.style.display = "none";
+        })
+    })
+}*/
+
 function displayweatherNow(data) {
-    const tempNowValue = data.main.temp;
+    const tempNowData = data.main.temp;
+    const humidNowData = data.main.humidity;
     const descriptionNow = data.weather[0].description;
     const iconCode = data.weather[0].icon;
-    const iconid = `https://openweathermap.org/img/w/${iconCode}.png`;
-    tempNow.textContent = tempNowValue;
+    /* const iconid = `https://openweathermap.org/img/w/${iconCode}.png`;*/
+    tempNow.textContent = tempNowData;
     weatherNowDesc.textContent = descriptionNow;
-    imageicon.src = iconid;
+    /* imageNow.src = iconid;*/
+    humidNow.textContent = humidNowData;
+
 
 }
 
-apiweatherFetch(weatherUrl);*/
+document.addEventListener("DOMContentLoaded", () => {
+    apiweatherFetch(weatherUrl);
+})
+
+
+
+const weatherForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=0.4475271339059875&lon=33.20316576377895&appid=1b38869f1c347522d45b8c33cd852ef5";
+async function forecastApiweatherFetch(url) {
+    let response = await fetch(url);
+    if (response.ok) {
+        let data = await response.json();
+        displayweatherForecast(data);
+    }
+}
+
+function displayweatherForecast(data) {
+    let daycontainer = document.querySelector(".daycontainer");
+
+    data.list.forEach(weather => {
+        let dateTime = new Date(weather.dt * 1000);
+        let localDateTime = dateTime.toLocaleString()
+        let date = dateTime.toDateString();
+        let dateToday = new Date();
+        let today = dateToday.toDateString();
+        let hours = new Date(localDateTime).getHours()
+
+        if ((date !== today) && (hours == 14)) {
+
+            let mainTemperature = document.querySelector('.main-temp');
+            let mainDescription = document.querySelector('.description');
+            let mainWeather = document.querySelector('.main-weather');
+            let forecastHumidity = document.querySelector('.forecast-humidity');
+            let imageNow = document.querySelector(".weather-icon");
+            /*weatherparaTime.classList.add("mode-weather");
+            weatherparaTemp.classList.add("mode-weather");
+            weatherparaDesc.classList.add("mode-weather");*/
+            let forecastIconCode = weather.weather[0].icon;
+            mainTemperature.textContent = `Temperature: ${weather.main.temp}`;
+            mainDescription.textContent = ` Description: ${weather.weather[0].description}`;
+            mainWeather.textContent = `${weather.weather[0].main}`;
+            forecastHumidity.textContent = `${weather.main.humidity}`;
+            imageNow.src = `https://openweathermap.org/img/w/${forecastIconCode}.png`;
+
+        }
+    })
+}
+
+
+forecastApiweatherFetch(weatherForecastUrl);
+/*
+addEventListener("DOMContentLoaded", () => {
+
+let dayDate = new Date();
+let dayOfWeek = dayDate.getDay();
+if (dayOfWeek == 0 || dayOfWeek == 6 || dayOfWeek == 1) {
+
+let eventBanner = document.querySelector("#eventBanner");
+eventBanner.style.display = "flex";
+eventBanner.style.color = "black";
+let eventPara = document.createElement("p");
+let eventButton = document.createElement("button");
+eventPara.textContent = "You are invited to Zion of Jinja chamber of commerce meet and greet on Wednesday at 7:00 p.m.";
+eventButton.textContent = `X`;
+eventPara.classList.add("event-para");
+eventButton.classList.add("event-button");
+eventBanner.appendChild(eventPara);
+eventBanner.appendChild(eventButton);
+
+eventButton.addEventListener("click", () => {
+eventBanner.style.display = "none";
+})
+
+}
+
+}) */
